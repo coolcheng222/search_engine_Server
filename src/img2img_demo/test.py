e2f7d5d3c665ae05996eba963dae2037ffddcad4
coolcheng222 <wo345@>
2023-11-08 19:27:57 +0800

    before demo

---
+++ src/img2img_demo/test.py
@@ -0,0 +1,19 @@
+from annoy import AnnoyIndex
+import random
+from tqdm import tqdm
+
+f = 2048  # Length of item vector that will be indexed
+
+t = AnnoyIndex(f, 'angular')
+for i in tqdm(range(100000)):
+    v = [random.gauss(0, 1) for z in range(f)]
+    t.add_item(i, v)
+
+t.build(10) # 10 trees
+t.save('test.ann')
+
+# ...
+
+u = AnnoyIndex(f, 'angular')
+u.load('test.ann') # super fast, will just mmap the file
+print(u.get_nns_by_item(0, 1000)) # will find the 1000 nearest neighbors
