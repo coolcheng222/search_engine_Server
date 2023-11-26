83de56014e69bbef1c3a0218bbcf66ad8e87c987
FelixK <43415565+Felix-Ku@users.noreply.github.com>
2023-11-26 16:36:29 +0800

    Add image encode function, updated flask response

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
+import io
+import os
+from base64 import encodebytes
+from PIL import Image
 from flask import Flask, jsonify, request
+from flask_cors import *
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
 from preprocessor.HDF5DataManager import HDF5DataManager
@@ -28,18 +34,38 @@ img2fea = BaseImage2Feature(device)
 searcher = Searcher(img2fea,fileManager,annoyManager)
 searcher.init()
 
+# Encode image into bytes
+def get_response_image(image_path):
+    pil_img = Image.open(image_path, mode='r') 
+    byte_arr = io.BytesIO()
+    pil_img.save(byte_arr, format='PNG') 
+    encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii') 
+    return encoded_img
+
 # TODO: elaborate the api
 @app.route('/search', methods=['POST'])
 def hello():
+
     try:
-        file = request.files['image']
-        
+        file = request.files.get('image', '')
+        size = request.form['size']
+        print(f'Search file numbers: {size}')
+
         image_data = file.read()
         if len(image_data) == 0:
             return jsonify({"code":1,"msg": "No image received"})
         image = toCv2(image_data)
-        imgs,dis = searcher.search(image,100)
-        return jsonify(imgs)
+        imgs,dis = searcher.search(image,int(size))
+        encoded_imges = []
+        for image_path in imgs:
+            temp_image_dict = {}
+            temp_encode = "data:image/jpg;base64," + get_response_image(os.path.join(root_directory,image_path))
+            temp_image_dict["largeURL"] = temp_encode
+            temp_image_dict["thumbnailURL"] = temp_encode
+            temp_image_dict["width"] = 900
+            temp_image_dict["height"] = 900
+            encoded_imges.append(temp_image_dict)
+        return jsonify(encoded_imges)
     except Exception as e:
         print(e)
         return jsonify({"code": 1,"msg": "error"})
